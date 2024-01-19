3ab241cfa266504ef87e7d0e6bfe5e8f54f9a687
João Ferreira <joaobatistacomunic@hotmail.com>
2024-01-19 18:27:31 -0300

    RepoScreen refactoring

---
+++ src/components/RepoDetails.tsx
@@ -0,0 +1,51 @@
+import { Repository } from '../github/api';
+
+type RepoDetailsProps = {
+  repo: Repository;
+};
+
+function RepoDetails({ repo }: RepoDetailsProps) {
+  return (
+    <div>
+      <h1>{repo.name}</h1>
+      <p>
+        @{repo.full_name}{' '}
+        <a href={repo.html_url} target="_blank" rel="noreferrer">
+          Ver no GitHub
+        </a>
+      </p>
+      {repo.homepage && (
+        <p>
+          <a href={repo.homepage} target="_blank" rel="noreferrer">
+            {repo.homepage}
+          </a>
+        </p>
+      )}
+      <p>{repo.description}</p>
+      <p>
+        Licença:{' '}
+        {repo.license ? (
+          <a href={repo.license.url} target="_blank" rel="noreferrer">
+            {repo.license.name}
+          </a>
+        ) : (
+          'Nenhuma'
+        )}
+      </p>
+      <p>Estrelas: {repo.stargazers_count}</p>
+      <p>Forks: {repo.forks}</p>
+      <p>Observadores: {repo.watchers_count}</p>
+      <p>Issues: {repo.open_issues_count}</p>
+
+      <p>Linguagem: {repo.language}</p>
+      <p>Tópicos:</p>
+      <ul>
+        {repo.topics.map((topic) => (
+          <li key={topic}>{topic}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default RepoDetails;
--- src/screens/RepoScreen.tsx
+++ src/screens/RepoScreen.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
+import RepoDetails from '../components/RepoDetails';
 import { GlobalContext } from '../context/GlobalContext';
 
 function RepoScreen() {
@@ -31,47 +32,7 @@ function RepoScreen() {
     return <p>Repositório não encontrado</p>;
   }
 
-  return (
-    <div>
-      <h1>{repo.name}</h1>
-      <p>
-        @{repo.full_name}{' '}
-        <a href={repo.html_url} target="_blank" rel="noreferrer">
-          Ver no GitHub
-        </a>
-      </p>
-      {repo.homepage && (
-        <p>
-          <a href={repo.homepage} target="_blank" rel="noreferrer">
-            {repo.homepage}
-          </a>
-        </p>
-      )}
-      <p>{repo.description}</p>
-      <p>
-        Licença:{' '}
-        {repo.license ? (
-          <a href={repo.license.url} target="_blank" rel="noreferrer">
-            {repo.license.name}
-          </a>
-        ) : (
-          'Nenhuma'
-        )}
-      </p>
-      <p>Estrelas: {repo.stargazers_count}</p>
-      <p>Forks: {repo.forks}</p>
-      <p>Observadores: {repo.watchers_count}</p>
-      <p>Issues: {repo.open_issues_count}</p>
-
-      <p>Linguagem: {repo.language}</p>
-      <p>Tópicos:</p>
-      <ul>
-        {repo.topics.map((topic) => (
-          <li key={topic}>{topic}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <RepoDetails repo={repo} />;
 }
 
 export default RepoScreen;
